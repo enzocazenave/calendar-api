5bde46db9e6589e70777051c25b476ea9434812a
enzocazenave <encaze24@hotmail.com>
2022-09-02 15:55:15 -0300

    Email exist validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,23 +3,35 @@ const User = require('../models/User');
 
 const createUser = async(req, res = response) => {
 
-    //const { name, email, password } = req.body;
+    const { email, password } = req.body;
 
     try {
-        const user = new User(req.body);
+        let user = await User.findOne({ email });
+        
+        if (user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El email esta en uso'
+            });
+        }
+
+        user = new User(req.body);
         await user.save();
 
         res.status(201).json({
             ok: true,
-            msg: ' ',
+            uid: user.id,
+            name: user.name
         });
     } catch(error) {
+
         console.log(error);
 
         res.status(500).json({
             ok: false,
             msg: 'Por favor hable con el administrador'
         })
+
     }
 }
 
