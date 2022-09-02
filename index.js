fddf5b3a388cb83b319fb3ac1d5f0184a2275300
enzocazenave <encaze24@hotmail.com>
2022-09-02 14:24:47 -0300

    Creating auth routes

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 require('dotenv').config();
 
-// CREA EL SERVIDOR DE EXPRESS
 const app = express();
 
-// DIRECTORIO PUBLICO
 app.use(express.static('public'));
 
-// ESCUCHAR
+app.use('/api/auth', require('./routes/auth'));
+
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${ process.env.PORT }`);
 });
