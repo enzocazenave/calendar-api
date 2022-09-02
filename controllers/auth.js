6968d9f577dfe7d4e2492bf9d71c4b2561ff4c11
enzocazenave <encaze24@hotmail.com>
2022-09-02 14:47:35 -0300

    Getting request.body

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,34 @@
 const { response } = require('express');
 
 const createUser = (req, res = response) => {
+
+    const { name, email, password } = req.body;
+
+    if (name.length < 5) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'El nombre debe tener 5 letras o mas'
+        })
+    }
+
     res.json({
         ok: true,
-        msg: 'register'
+        msg: 'register',
+        name,
+        email,
+        password
     });
 }
 
 const loginUser = (req, res = response) => {
+
+    const { email, password } = req.body;
+
     res.json({
         ok: true,
-        msg: 'login'
+        msg: 'login',
+        email,
+        password
     })
 }
 
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 require('dotenv').config();
 
+// Crear el servidor de express
 const app = express();
 
+// Directorio publico
 app.use(express.static('public'));
 
+// Lectura y parseo del body
+app.use(express.json());
+
+// Rutas
 app.use('/api/auth', require('./routes/auth'));
 
+// Escuchar peticiones
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${ process.env.PORT }`);
 });
