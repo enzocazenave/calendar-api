36523ed5d50c8f4dd27c66daacce77630f42f307
enzocazenave <encaze24@hotmail.com>
2022-09-02 16:22:12 -0300

    Add generate jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require('express');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const { generateJWT } = require("../helpers/jwt");
 
 const createUser = async(req, res = response) => {
 
@@ -18,16 +19,18 @@ const createUser = async(req, res = response) => {
 
         user = new User(req.body);
 
-        // Encriptar contraseña
         const salt = bcrypt.genSaltSync();
         user.password = bcrypt.hashSync(password, salt);
 
         await user.save();
 
+        const token = await generateJWT(user.id, user.name);
+
         res.status(201).json({
             ok: true,
             uid: user.id,
-            name: user.name
+            name: user.name,
+            token
         });
     } catch(error) {
         console.log(error);
@@ -53,7 +56,6 @@ const loginUser = async(req, res = response) => {
             })
         }
 
-        // Confirmar password  
         const validPassword = bcrypt.compareSync(password, user.password);
 
         if (!validPassword) {
@@ -63,11 +65,13 @@ const loginUser = async(req, res = response) => {
             });
         }
 
-        // Generar nuestro JWT
+        const token = await generateJWT(user.id, user.name);
+
         res.json({
             ok: true,
             uid: user.id,
-            name: user.name
+            name: user.name,
+            token
         })
     } catch(error) {
         console.log(error);
