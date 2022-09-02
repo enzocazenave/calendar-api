5b84b70f3564aaef9e5caa70bebc1e1a79714de9
enzocazenave <encaze24@hotmail.com>
2022-09-02 17:30:20 -0300

    Add custom check validator

--- routes/events.js
+++ routes/events.js
@@ -4,6 +4,9 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { isDate } = require('../helpers/isDate');
+const { fieldValidator } = require("../middlewares/field-validators");
 const { validateJWT } = require('../middlewares/jwt-validate');
 const { getEvents, createEvent, updateEvent, deleteEvent } = require('../controllers/events');
 
@@ -12,7 +15,18 @@ const router = Router();
 router.use(validateJWT);
 
 router.get('/', getEvents);
-router.post('/', createEvent);
+
+router.post(
+    '/',
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'Fecha de finalizacion es obligatoria').custom(isDate),
+        fieldValidator
+    ], 
+    createEvent
+);
+
 router.put('/:id', updateEvent);
 router.delete('/:id', deleteEvent);
 
